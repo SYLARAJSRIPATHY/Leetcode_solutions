d2e34fd7fac0a3e760a206a2c83fb4dcecdec6d8
SYLARAJSRIPATHY <146091916+SYLARAJSRIPATHY@users.noreply.github.com>
2025-04-28 19:08:07 +0530

    Time: 79 ms (57.90%) | Memory: 19 MB (90.09%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        min_price = float('inf') 
+        max_profit = 0           
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            else:
+                profit = price - min_price
+                max_profit = max(max_profit, profit)
+        return max_profit
+sol = Solution()
+
+print(sol.maxProfit([7,1,5,3,6,4]))
+print(sol.maxProfit([7,6,4,3,1]))
+
+        
